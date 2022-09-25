702fb0418c534a3a92094116313edc7420d47590
dev <dev@infrastigator.com>
2022-09-25 21:37:47 +1000

    Added ability to store resulting JSON in local folder

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ target_company = Company(company_number=ch_number)
 analysis = Analysis(target_company)
 analysis.get_api_data()
 
+# Store resulting aggregated JSON in local folder
+with open("output/"+ch_number+".json", "w") as outfile:
+    outfile.write(target_company.to_json())
+
 # TODO destination folder for binaries
 # analysis.get_api_data(download_binary=True)
 
-print(target_company.to_json())
-print("\n")
+# print(target_company.to_json())
 
 print(target_company.company_name)
 print("\n")
